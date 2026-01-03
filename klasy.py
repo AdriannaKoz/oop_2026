99c80b7e5a753d441ba637a7905addfe99e772d1
AdriannaKoz <a.a.kozimor@gmail.com>
2026-01-03 11:40:20 +0000

    Add new methods

--- klasy.py
+++ klasy.py
@@ -4,19 +4,37 @@ from tokenize import String
 
 class Czlowiek:
     #Istota
+    #Atrybuty klasy
     gatunek = "Homo sapiens"
-    def __init__(self, imie):
+    def __init__(self, imie, plec): #Atrybuty OOBIEKTU (skladniki potrawy)
         # Konstruktor
         # Akt istnienia
         print(f"Niech powstanie czlowiek o imieniu {imie}")
         self.imie = imie
+        self.plec = plec
         #adam.imie = "Adam"
         #ewa.imie = "Ewa"
+
+    #Metoda
+    #Moznosc (mozliwosc), zdolnosc, umiejetnosc
+    def przedstaw_sie(self):
+        print(f"Dzien dobry, am na imie {self.imie} i jestem", end="")
+        if self.plec == "M":
+            print("mezczyzna")
+        else:
+            print("kobieta")
+
+    def przedstaw(self, osoba):
+        print(f"Oto {osoba.imie}")
 
 #Powstanie z obiektu
 #Gotowanie z przepisu
 
-adam = Czlowiek("Adam")
-ewa = Czlowiek("Ewa")
+adam = Czlowiek("Adam", "M")
+ewa = Czlowiek("Ewa", "K")
+
 print(adam.gatunek)
 print(ewa.gatunek)
+
+adam.przedstaw_sie()
+ewa.przedstaw(adam)
