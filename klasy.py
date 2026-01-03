c3fbf6002fdbf4abcc4ccfa25b621c2626f43437
AdriannaKoz <a.a.kozimor@gmail.com>
2026-01-03 12:38:05 +0000

    Add baw_sie method

--- klasy.py
+++ klasy.py
@@ -18,7 +18,7 @@ class Czlowiek:
     #Metoda
     #Moznosc (mozliwosc), zdolnosc, umiejetnosc
     def przedstaw_sie(self):
-        print(f"Dzien dobry, am na imie {self.imie} i jestem", end="")
+        print(f"Dzien dobry, am na imie {self.imie} i jestem ", end="")
         if self.plec == "M":
             print("mezczyzna")
         else:
@@ -27,14 +27,29 @@ class Czlowiek:
     def przedstaw(self, osoba):
         print(f"Oto {osoba.imie}")
 
+class Dziecko(Czlowiek):
+    def baw_sie(self):
+        print("Ale zabawa, juhuu!!!")
+
+    def przedstaw_sie(self):
+        print(f"Cesc, mam na imie {self.imie} i jestem ", end="")
+        if self.plec == "M":
+            print("chlopcem")
+        else:
+            print("dziewcynka")
+
+
 #Powstanie z obiektu
 #Gotowanie z przepisu
 
 adam = Czlowiek("Adam", "M")
 ewa = Czlowiek("Ewa", "K")
+kain = Dziecko("Kain", "M")
 
 print(adam.gatunek)
 print(ewa.gatunek)
 
 adam.przedstaw_sie()
 ewa.przedstaw(adam)
+kain.baw_sie()
+kain.przedstaw_sie()
