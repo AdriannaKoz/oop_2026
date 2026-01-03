f72b5c877d057d08ad6bbd348cf43b13a74ad8c6
AdriannaKoz <a.a.kozimor@gmail.com>
2026-01-03 10:27:33 +0000

    Added new information about Czlowiek

--- klasy.py
+++ klasy.py
@@ -1,16 +1,22 @@
 #Klasa = Szablon, Przepis
+from tokenize import String
+
 
 class Czlowiek:
     #Istota
     gatunek = "Homo sapiens"
-    def __init__(self):
+    def __init__(self, imie):
         # Konstruktor
         # Akt istnienia
-        print("Niech powstanie czlowiek")
-        pass
+        print(f"Niech powstanie czlowiek o imieniu {imie}")
+        self.imie = imie
+        #adam.imie = "Adam"
+        #ewa.imie = "Ewa"
 
 #Powstanie z obiektu
 #Gotowanie z przepisu
 
-adam = Czlowiek()
+adam = Czlowiek("Adam")
+ewa = Czlowiek("Ewa")
 print(adam.gatunek)
+print(ewa.gatunek)
